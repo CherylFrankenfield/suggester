2f7910f28b09047053b9b0fd484997ccb5fcd0db
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-20 14:46:01 -0700

    Adjust branch logic to include an alert

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,31 @@ $(document).ready(function() {
 
     var codetrack = add(question1, question2, question3, question4, question5);
 
+//First round of logic without adding dropdown question: Choose your answer below. //
+
+    // if (codetrack >= 5 && codetrack <= 8) {
+    //   $("#ruby").show();
+    // }
+    //   else if (codetrack >= 9 && codetrack <= 12) {
+    //   $("#design").show();
+    // }
+    //   else {
+    //   $("#java").show();
+    // }
+
+// Because I added a value of zero for the dropdown header, changed logic to include an alert if user accidentally selects this column header. //
+
     if (codetrack >= 5 && codetrack <= 8) {
       $("#ruby").show();
     }
       else if (codetrack >= 9 && codetrack <= 12) {
       $("#design").show();
     }
-      else {
+      else if (codetrack >= 13 && codetrack <= 15) {
       $("#java").show();
+    }
+      else {
+        alert("Oops! A selection was skipped. Please make all selections and hit submit.");
     }
   });
 });
